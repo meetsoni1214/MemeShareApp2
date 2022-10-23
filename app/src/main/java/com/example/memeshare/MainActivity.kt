a9a174f6c22e1c4eb75c02a57f369cc75f494631
Meet <meetsoni1214@gmail.com>
2022-10-23 11:41:42 +0530

    This is my second commit

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -15,6 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.d("MAINACTIVITY", "This is my message")
     }
 }
